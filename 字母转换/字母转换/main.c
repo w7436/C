ab1af937648f0c3585dfc3d109f5656683029e9d
w7436 <3239741254@qq.com>
2019-07-19 14:57:01 +0800

    代码上传

---
+++ 字母转换/字母转换/main.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+#include "stdio.h"
+#include "stdlib.h"
+int main() {
+	char m = ' ';
+	while (m != '!') {
+		scanf("%c", &m);
+		if (m >= 65 && m <= 90) {
+			m = m + 32;
+			printf("%c\n", m);
+		}
+		else if (m >= 97 && m <= 122) {
+			m = m - 32;
+			printf("%c\n", m);
+		}
+	}
+	system("pause");
+	return 0;
+}
