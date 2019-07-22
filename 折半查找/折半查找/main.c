baf422b5beb5778bbede0469d95e4a21d8eb42f5
w7436 <3239741254@qq.com>
2019-07-22 19:36:57 +0800

    代码上传

--- 折半查找/折半查找/main.c
+++ 折半查找/折半查找/main.c
@@ -1,28 +1,34 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
-void BinarySearch(int arr[], int right, int result) {
+int BinarySearch(int arr[], int right, int result) {
 	int  left = 0;
 	int middle = (left + right) / 2;
-	while (arr[middle] !=result) {
-		if (arr[middle]<result) {    //´ÓÓÒ±ßÕÒ
-			middle = (middle + right)/2;
-			continue;
+	while (right - left > 1) {
+		if (arr[middle] < result) {    //´ÓÓÒ±ßÕÒ
+			left = middle;
+			middle = (left +right) / 2;
 		}
-		if (arr[middle] >=result) {  //´Ó×ó±ßÕÒ
-			middle =( middle + left)/2;
-			continue;
+		if (arr[middle] > result) {  //´Ó×ó±ßÕÒ
+			right = middle;
+			middle = (right + left)/2;
 		}
+		if (arr[middle] == result)
+			break;
 	}
-	printf("%d\n", middle);
+	if (arr[middle] == result)
+		return middle;
+	else
+		return -1;
 }
 int main() {
-	int result = 0;
+	int result = 0,m=0;
 	int arr[] = { 1,3,6,7,9,14,35,67 };
 	int right = sizeof(arr) / sizeof arr[0]-1;
 	printf("ÇëÊäÈëÄãÑ°ÕÒµÄÊı×Ö£º");
 	scanf("%d", &result);
-	BinarySearch(arr, right, result);
+	m=BinarySearch(arr, right, result);
+	printf("%d\n", m);
 	return 0;
 	
 }
