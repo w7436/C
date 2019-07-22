e412ad3374e13f62de0fad78a5c70d5cbc6780e8
w7436 <3239741254@qq.com>
2019-07-22 14:55:27 +0800

    代码上传

--- 含9的数/含9的数/main.c
+++ 含9的数/含9的数/main.c
@@ -1,15 +1,20 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+int Function(int n) {
+	int count = 0;
+	if (n%10==9) {
+		count++;
+	}
+	if (n/10==9) {
+		count++;
+	}
+	return count;
+}
 int main() {
 	int count=0;
 	for (int n = 1; n <= 100;n++) {
-		if (n%10==9) {
-			count++;
-		}
-		if ((n / 10 == 9)) {
-			count++;
-		}
+		count += Function(n);
 	}
 	printf("%d\n", count);
 	system("pause");
