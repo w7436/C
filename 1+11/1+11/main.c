551956c7d04a236f14a90d837fc7af75b4aaf654
w7436 <3239741254@qq.com>
2019-07-22 14:54:21 +0800

    代码上传

--- 1+11/1+11/main.c
+++ 1+11/1+11/main.c
@@ -1,6 +1,20 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <math.h>
+//求Sn = a + aa + aaa + aaaa + aaaaa的前n项之和，其中a是一个数字，
+//例如：2 + 22 + 222 + 2222 + 22222
+int main() {
+	int a = 2, n = 0, sum = 0, tmp = 0;
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++) {
+		tmp = tmp * 10 + a;  //2 22 222
+		sum += tmp;
+	}
+	printf("%d\n", sum);
+	return 0;
+}
+
+#if 0
 int main() {
 	int n=0,m=0,sum=0,a[256]={0};
 	printf("请输入需计算的数：");
@@ -18,4 +32,5 @@ int main() {
 	}
 	printf("%d\n", sum);
 	return 0;
-}
+
+#endif
