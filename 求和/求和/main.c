bdc19992ea53075bd6c2ca97ac6ec40472077d84
w7436 <3239741254@qq.com>
2019-07-22 14:56:16 +0800

    1-1/2+1/3

--- 求和/求和/main.c
+++ 求和/求和/main.c
@@ -3,10 +3,10 @@
 #include<stdlib.h>
 int main() {
 	double sum = 0.0;
-	double j = -1;
+	double j = -1;  //int flag=1;
 	for (int i = 1; i <= 10;i++) {
 		j =  j * (-1);
-		sum =sum+ j/i;
+		sum =sum+ j/i; //sum=sum+flag*1.0/i;  flag=-flag;
 	}
 	printf("%lf\n", sum);
 	system("pause");
