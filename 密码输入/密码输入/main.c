3e8e447a5f7b8c7ff87c5f98bc81c385e8fb361d
w7436 <3239741254@qq.com>
2019-07-23 15:30:18 +0800

    输入密码，最多输三次

--- 密码输入/密码输入/main.c
+++ 密码输入/密码输入/main.c
@@ -2,6 +2,28 @@
 #include "stdlib.h"
 #include "stdio.h"
 #include <string.h>
+int main()
+{
+	char password[20] = { 0 };
+	int count = 3;
+	while (count != 0)
+	{
+		printf("请输入你的password：");
+		scanf("%s", password);   //scanf  遇到空格，回车停止输入
+		if (strcmp(password, "123456") == 0)//"123456"
+		{
+			printf("登录成功\n");
+			break;
+		}
+		else
+		{
+			count--;
+			printf("你还有%d次机会!\n", count);
+		}
+	}
+	return 0;
+}
+#if 0
 int main() {
 	char a[10], b[10] = "hello";
 	printf("请输入密码：");
@@ -14,8 +36,9 @@ int main() {
 		}
 		else
 			printf("请重新输入：");
-		m++;
+			m++;
 	}
 	system("pause");
 	return 0;
 }
+#endif
