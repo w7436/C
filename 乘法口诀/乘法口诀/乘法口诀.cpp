75f7322b18100ec70b3b25a7fbd73dab09f131a8
w7436 <3239741254@qq.com>
2019-07-24 16:56:49 +0800

    代码上传

--- 乘法口诀/乘法口诀/乘法口诀.cpp
+++ 乘法口诀/乘法口诀/乘法口诀.cpp
@@ -1,14 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include "stdlib.h"
-int main() {
-	for (int i = 1; i <= 9;i++) {
+void Mtp_table(int n) {
+	int j =0;
+	for (int i = 1; i <= n;i++) {
 		for (int j = 1; j <= i;j++) {
-			printf("%d*%d=%d\t", j, i,i*j);
-			if (j==i) {
-				printf("\n");
-			}
-		}	
+			printf("%d*%d=%d\t", j, i, i*j);
+		}
+	printf("\n");
 	}
+}
+int main() {
+	int num = 0;
+	printf("请输入需打印的乘法口诀：");
+	scanf("%d", &num);
+	Mtp_table(num);
 	system("pause");
 	return 0;
 }
