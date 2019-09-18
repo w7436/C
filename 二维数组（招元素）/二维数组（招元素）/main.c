c71ab63dfea9f3565d2e48efa646948fe3acd97b
w7436 <3239741254@qq.com>
2019-09-18 08:53:48 +0800

    代码上传

--- 二维数组（招元素）/二维数组（招元素）/main.c
+++ 二维数组（招元素）/二维数组（招元素）/main.c
@@ -1,35 +1,55 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-int  Find(int arr[][4],int input) {
-	int i = 0,j=0;
-	for (i = 0; i < 4; i++) {
-		for (j = 0; j < 4; j++) {
-			if (input == arr[j]) {
-				return 1;
-			}
+/*杨氏矩阵
+有一个二维数组.
+数组的每行从左到右是递增的，每列从上到下是递增的.
+在这样的数组中查找一个数字是否存在。
+时间复杂度小于O(N);
+
+数组：
+1 2 3
+2 3 4
+3 4 5
+
+
+1 3 4
+2 4 5
+4 5 6
+
+1 2 3
+4 5 6
+7 8 9
+*/
+int  Find(int arr[][3], int input) {
+	int i = 0;
+	int j = 2;
+	while (i<3&&j>=0) {
+		if (arr[i][j]>input)
+		{
+			j--;
+		}
+		else if (arr[i][j] == input) {
+			return 1;
+		}
+		else {
+			i++;
 		}
 	}
 	return 0;
 }
 int main() {
 	int  j = 0,input=0,flag=0;
-	int arr[4][4] = { 0 };
-	//二维数组的输入
-	for (int i = 0; i < 4;i++) {
-		for (j = 0; j < 4;j++) {
-			scanf("%d", arr[j]);
-		}
-	}
+	int arr[3][3] = { 1,2,3,2,3,4,3,4,5};
 	printf("请输入你寻找的数字：");
 	scanf("%d", &input);
-	flag = Find(arr, input);
-	if (flag=0) {
-		printf("NO Find\n");
+	int flg = Find(arr, input);
+	if (flg==1) {
+		printf("FIND!");
 	}
 	else
 	{
-		printf("Find\n");
+		printf("NO FIND");
 	}
 	system("pause");
 	return 0;
