e5bd4ff7a477ad179c899855b92dcde184e77901
w7436 <3239741254@qq.com>
2019-09-19 21:45:48 +0800

    代码上传

--- 多项式的相加/多项式的相加/List.h
+++ 多项式的相加/多项式的相加/List.h
@@ -1,1 +1,17 @@
 #pragma once
+#include <stdio.h>
+#include<stdlib.h>
+
+typedef struct poly {
+	int exp;
+	int coef;
+	struct poly *next;
+}Node, *PLinklist;
+
+//初始化
+int Init(PLinklist *head);
+//创建链表（尾插）
+int CreatList(PLinklist *head);
+void Add(PLinklist L1, PLinklist L2);
+
+
