9e38359cca64d6e23acd271feaa62f4e658d43c6
w7436 <3239741254@qq.com>
2019-11-22 21:32:55 +0800

    有序二维数组的查找

--- 二维数组（招元素）/二维数组（招元素）/main.c
+++ 二维数组（招元素）/二维数组（招元素）/main.c
@@ -1,6 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+
 /*杨氏矩阵
 有一个二维数组.
 数组的每行从左到右是递增的，每列从上到下是递增的.
@@ -21,36 +22,43 @@
 4 5 6
 7 8 9
 */
-int  Find(int arr[][3], int input) {
-	int i = 0;
-	int j = 2;
-	while (i<3&&j>=0) {
-		if (arr[i][j]>input)
-		{
-			j--;
-		}
-		else if (arr[i][j] == input) {
-			return 1;
-		}
-		else {
-			i++;
-		}
+
+//int  Find(int arr[][3], int input) {
+//	int i = 0;
+//	int j = 2;
+//	while (i<3&&j>=0) {
+//		if (arr[i][j]>input)
+//		{
+//			j--;
+//		}
+//		else if (arr[i][j] == input) {
+//			return 1;
+//		}
+//		else {
+//			i++;
+//		}
+//	}
+//	return 0;
+//}
+
+void Find(int *arr,int *px,int *py,int len,int key) {   //数组指针
+	int row = 2;
+	int col = len - 1;
+	while (  (*(arr)+row)   ) {
 	}
-	return 0;
+
+}
+void show(int *arr, int col, int row) {
+	for (int ) {
+		printf("%d ", arr[]);
+	}
 }
 int main() {
 	int  j = 0,input=0,flag=0;
 	int arr[3][3] = { 1,2,3,2,3,4,3,4,5};
 	printf("请输入你寻找的数字：");
 	scanf("%d", &input);
-	int flg = Find(arr, input);
-	if (flg==1) {
-		printf("FIND!");
-	}
-	else
-	{
-		printf("NO FIND");
-	}
+	
 	system("pause");
 	return 0;
 }
